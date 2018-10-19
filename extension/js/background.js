8e414196973481c9f401bcbce7bbd2a27ce574ce
called-d <called.0xd@gmail.com>
2018-10-19 22:11:46 +0900

    mod: mainWindow object on background.js

--- extension/js/background.js
+++ extension/js/background.js
@@ -6,19 +6,38 @@ let fl = new Fullerene()
 
 const MENU_ID_OPEN_FULLERENE_WINDOW = "openFullereneWindow";
 
-var mainWindow;
+const mainWindow = {
+    _w: null,
+    get() {
+        return new Promise((resolve, reject) => {
+            if (!this._w) reject("no main window");
+            chrome.windows.get(this._w.id, w => {
+                if (chrome.runtime.lastError) reject(chrome.runtime.lastError);
+                resolve(w);
+            });
+        });
+    },
+    open() {
+        this.get().then(w => {
+            chrome.windows.update(w.id, {focused: true});
+        }).catch(_ => {
+            chrome.windows.create(
+                {
+                    url: "extension/main.html",
+                    width: 480,
+                    height: 1024,
+                    type: "popup",
+                    focused: true,
+                },
+                w => { self._w = w }
+            );
+        });
+    }
+};
+
 const contextMenuHandlers = {
     [MENU_ID_OPEN_FULLERENE_WINDOW]: () => {
-        chrome.windows.create({
-            url: "extension/main.html",
-            width: 480,
-            height: 1024,
-            type: "popup",
-            focused: true,
-        },
-        (window) => {
-            mainWindow = window;
-        });
+        mainWindow.open();
     },
 };
 
